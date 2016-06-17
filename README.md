e35bc175614bcd224edbc5057e3983a4698476e3
Julian Higman <jhigman@pobox.com>
2016-06-17 15:26:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Newsletters
+===========
+
+Find newsletters on the West Bridgford schools sites and copy them to Dropbox
+
+
+The code relies on some libraries : requests, BeautifulSoup, dropbox:
+
+    sudo pip3 install beautifulsoup4
+    sudo pip3 install dropbox
+
+You'll need to setup a Dropbox access token in the environment:
+
+    export DROPBOX_ACCESS_TOKEN=[access token]
+
+
---
+++ sync.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import os
+import dropbox
+import requests
+import bs4
+
+def list_dropbox():
+    dropbox_access_token = os.environ['DROPBOX_ACCESS_TOKEN']
+    dbx = dropbox.Dropbox(dropbox_access_token)
+    return [ entry.name for entry in dbx.files_list_folder('').entries ]
+
+def save_to_dropbox(prefix, path, url):
+    dropbox_access_token = os.environ['DROPBOX_ACCESS_TOKEN']
+    dbx = dropbox.Dropbox(dropbox_access_token)
+    dbx.files_save_url('/' + path, url)
+
+def find_pdfs(url, prefix, filenames):
+    response = requests.get(url)
+    soup = bs4.BeautifulSoup(response.text)
+    for link in soup.find_all('a'):
+        if link.get('href').endswith('.pdf'):
+            filename = prefix + ' : ' + link.text + '.pdf'
+            if filename not in filenames:
+                save_to_dropbox(prefix, filename, link.get('href'))
+                print('saved ' + filename)
+            else:
+                print('already got ' + filename)
+
+filenames = list_dropbox()
+
+find_pdfs('http://www.wbjs.com/stream/newsletters/full/1/-//', 'JUNIORS', filenames)
+find_pdfs('http://www.westbridgfordinfants.co.uk/stream/newsletters/full/1/-//', 'INFANTS', filenames)
+
+
